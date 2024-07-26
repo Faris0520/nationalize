f38a065edb916d7aa6ecdb8d2ccf50fab657b484
Faris0520 <faris.6dsdiaf@gmail.com>
2024-07-27 01:30:37 +0800

    final?

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Predict the Ethnicity of a Name, using Nationalize API
 
-
+<img src="image.png" width="60%">
--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 async function cekNama() {
     const nama = document.getElementById('nameInput').value;
-    const resultElement = document.getElementById('result');
+    const resultElement = document.getElementById('hasil');
 
     if (nama) {
         try {
             const response = await fetch(`https://api.nationalize.io/?name=${nama}`);
             const data = await response.json();
-            let negara = data.country[0].country_id;
-            if (data.gender) {
-                resultElement.innerHTML = `<strong>${nama}</strong> is from <strong>${negara}</strong> with <strong>${data.probability * 100}%</strong> certainty.`;
+            const countryList = await fetch('country.json').then(res => res.json());
+
+            if (data.country.length > 0) {
+                let countryId = data.country[0].country_id;
+                let negara = getCountryName(countryId, countryList);
+                let bendera = getFlagEmoji(countryId);
+
+                resultElement.innerHTML = `${bendera} <strong>${nama}</strong> is from <strong>${negara}</strong> with <strong>${(data.country[0].probability * 100).toFixed(2)}%</strong> certainty.`;
             } else {
-                resultElement.innerHTML = `Uh oh. <strong>${nama}</strong> is unknown to us`;
+                resultElement.innerHTML = `Uh oh. <strong>${nama}</strong> is unknown to us.`;
             }
         } catch (error) {
             resultElement.textContent = 'An error occurred while fetching the data.';
@@ -18,6 +23,16 @@ async function cekNama() {
     } else {
         resultElement.textContent = 'Please enter a name.';
     }
+}
+
+function getCountryName(countryId, countryList) {
+    return countryList[countryId] || 'Unknown Country';
+}
+
+function getFlagEmoji(countryCode) {
+    return countryCode
+        .toUpperCase()
+        .replace(/./g, char => String.fromCodePoint(127397 + char.charCodeAt()));
 }
 
 function kirim(event) {
